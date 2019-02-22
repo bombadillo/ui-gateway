fb0727e530587d5ecd9163baead77eb28ab78380
bombadillo <mumfordnsons@gmail.com>
2019-02-22 08:36:06 +0000

    Refactor UI retriever to remove hardcoded UI and use UI provider

--- src/services/component-provider/ui-retriever.service.ts
+++ src/services/component-provider/ui-retriever.service.ts
@@ -1,82 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { EndpointRetrieverService } from './endpoint-retriever.service';
 import { ComponentType } from 'src/enums/component-type.enum';
+import axios from 'axios';
 
 @Injectable()
 export class UIRetrieverService {
   constructor(private endpointRetrieverService: EndpointRetrieverService) {}
 
-  retrieveByToken(token: string, componentType: ComponentType) {
+  async retrieveByToken(token: string, componentType: ComponentType) {
     console.log(`loading UI for ${token}`);
-    console.log(componentType);
-    console.log(typeof componentType);
+
     var uiEndpoint = this.endpointRetrieverService.retrieveByToken(
       token,
       componentType,
     );
 
+    console.log(`retrieving UI from ${uiEndpoint}`);
+
     if (uiEndpoint) {
-      return `
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+      let response = await axios.get(uiEndpoint);
 
-      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">Tacos</h1>  
-      </div>
-      
-      <div class="container">
-        <div class="card-deck mb-3 text-center">
-
-          <div class="card mb-4 shadow-sm">
-            <div class="card-header">
-              <h4 class="my-0 font-weight-normal">🌮 Barbacoa 🌮</h4>
-            </div>
-            <div class="card-body">
-              <h1 class="card-title pricing-card-title">$6</h1>
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>20 flavours included</li>
-                <li>10 spices</li>     
-                <li>Ultimate enjoyment</li>         
-              </ul>
-              ${componentType === ComponentType.WebComponent &&
-                '<button type="button" class="btn btn-lg btn-block btn-primary">Get 🌮</button>'}              
-            </div>
-          </div>
-
-          <div class="card mb-4 shadow-sm">
-            <div class="card-header">
-              <h4 class="my-0 font-weight-normal">🌮 Chorizo 🌮</h4>
-            </div>
-            <div class="card-body">
-              <h1 class="card-title pricing-card-title">$9</h1>
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>30 slices of sausage</li>
-                <li>It'sa spicy boi</li>
-                <li>Stomach clenching</li>
-              </ul>
-              ${componentType === ComponentType.WebComponent &&
-                '<button type="button" class="btn btn-lg btn-block btn-primary">Get 🌮</button>'}              
-            </div>
-          </div>
-
-          <div class="card mb-4 shadow-sm">
-            <div class="card-header">
-              <h4 class="my-0 font-weight-normal">🌮 Chipotle 🌮</h4>
-            </div>
-            <div class="card-body">
-              <h1 class="card-title pricing-card-title">$14</h1>
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>Top 🌮 lovers only</li>
-                <li>Extreme strength</li>
-                <li>Priority toilet support</li>
-              </ul>
-              ${componentType === ComponentType.WebComponent &&
-                '<button type="button" class="btn btn-lg btn-block btn-primary">Get 🌮</button>'}              
-            </div>
-          </div>
-
-        </div>
-      </div>
-    `;
+      if (response.data) {
+        return response.data;
+      }
     }
 
     return null;
